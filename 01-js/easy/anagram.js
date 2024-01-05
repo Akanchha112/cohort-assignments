6bb69de98a82ffc1ef028f4adc922dc9bf5ddaed
Akanchha <sakanchha111@gmail.com>
2024-01-05 13:18:53 +0530

    Week 1 assign done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,33 @@
 */
 
 function isAnagram(str1, str2) {
-
+    let len;
+    let len2;
+    if(str1.length>=str2.length){
+      len=str1.toUpperCase()
+      len2=str2.toUpperCase()
+    }
+    else{
+      len=str2.toUpperCase()
+      len2=str1.toUpperCase()
+    }
+      for(let i=0;i<len.length;i++){
+        let a=true
+        for(let j=0;j<len2.length;j++){
+          if(len2[j]!=len[i]){
+            a=false
+          }
+          else{
+            a=true
+            break;
+          }
+          if(j==len2.length-1 && a==false){
+            return false
+          }
+        }
+      }
+      return true;
 }
-
+// a=isAnagram('abc!', '!bac')
+// console.log(a);
 module.exports = isAnagram;
--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,17 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let currtime= new Date()
+    a=new Date().getTime()
+    // console.log(new Date().getTime());
+    let sum=0
+    for(let i=0;i<n;i++){
+        sum+=i;
+    }
+    b=new Date().getTime()
+    // console.log(new Date().getTime());
+    return b-a;
 }
+console.log(calculateTime(100));
+console.log(calculateTime(100000));
+console.log(calculateTime(1000000000));
